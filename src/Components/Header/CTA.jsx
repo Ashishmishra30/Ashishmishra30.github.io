ce8f787691f6ec89ea4633563dc4e8a5751a06ee
Ashish Mishra <ashishmishra9467@gmail.com>
2023-03-25 13:49:31 +0530

    done

--- src/Components/Header/CTA.jsx
+++ src/Components/Header/CTA.jsx
@@ -3,7 +3,7 @@ import Ashish_Mishra_Resume from "../../assets/Ashish_Mishra_Resume.pdf";
 const CTA = () => {
     return (
         <div className="cta">
-            <a  id="resume-button-2" href={Ashish_Mishra_Resume} download className='btn'>Resume</a>
+            <a  id="resume-button-2 resume-link-2" href={Ashish_Mishra_Resume} download className='btn'>Resume</a>
             <a href="#contact" className='btn btn-primary'>Let's Talk</a>
         </div>
     )
