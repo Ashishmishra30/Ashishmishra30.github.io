ff8e227d15108d893395302ab05ba736a65535e6
Ashish Mishra <ashishmishra9467@gmail.com>
2023-05-09 15:14:06 +0530

    done

--- src/Components/Github/Calander.jsx
+++ src/Components/Github/Calander.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./calender.css";
+import GitHubCalendar from "react-github-calendar";
+
 const Calander = () => {
   return (
     <div>
@@ -12,14 +14,14 @@ const Calander = () => {
             <div>
               <img
                 id="github-streak-stats"
-                src="https://github-readme-streak-stats.herokuapp.com?user=Ashishmishra30&theme=dark"
+                src="https://github-readme-streak-stats.herokuapp.com?user=Ashishmishra30&theme=transparent"
                 alt="github-readme-streak-stats"
               />
             </div>
             <div>
               <img
                 id="github-stats-card"
-                src="https://github-readme-stats.vercel.app/api?username=Ashishmishra30&show_icons=true&theme=dark"
+                src="https://github-readme-stats.vercel.app/api?username=Ashishmishra30&show_icons=true&theme=transparent"
                 alt=""
               />
             </div>
@@ -27,7 +29,7 @@ const Calander = () => {
           <div class="github-lang-div">
             <img
               id="github-top-langs"
-              src="https://github-readme-stats.vercel.app/api/top-langs/?username=Ashishmishra30&layout=compact&theme=dark"
+              src="https://github-readme-stats.vercel.app/api/top-langs/?username=Ashishmishra30&layout=compact&theme=transparent"
               alt=""
             />
           </div>
@@ -38,9 +40,22 @@ const Calander = () => {
         </h2>
 
         <div id="calenderdiv" class="react-activity-calendar">
-          <div class="calendar" style={{marginTop:"30px",color:"white"}}>
+          <div
+            class="calendar"
+            id="cal"
+            style={{ marginTop: "30px", color: "white" }}
+          >
             {/* <!-- Loading stuff --> */}
-            Loading the data just for you.
+            {/* Loading the data just for you. */}
+            <GitHubCalendar
+              username="ashishmishra30"
+              blockSize={15}
+              blockMargin={5}
+              color="#c084f5"
+              fontSize={16}
+              colorScheme="light"
+              m={"auto"}
+            />
           </div>
         </div>
       </section>
